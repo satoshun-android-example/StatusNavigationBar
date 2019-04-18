98494323485787c45a013b7cc3f2c91d0a5d151e
SatoShun <shun.sato1@gmail.com>
2019-04-18 05:08:40 +0000

    add sample dialog

--- app/src/main/java/com/github/satoshun/example/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/example/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.github.satoshun.example
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.main_act.*
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main_act)
+    setSupportActionBar(toolbar)
+
+    SampleDialogFragment1()
+      .show(supportFragmentManager, "test")
+  }
+}
+
+internal class SampleDialogFragment1 : DialogFragment() {
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    val view = inflater.inflate(
+      R.layout.sample_dialog1,
+      container,
+      false
+    )
+    return view
   }
 }
